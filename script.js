f9c8094343fb4fe5b5e84d15a876b8ed02f26a3a
Michael Untalan <michael.untalan.742@gmail.com>
2024-04-25 21:46:25 -0400

    Added CSS Styles to the Index. Created NavBar, Background Image, tested Functionality of NavBar

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ const navItems = [
 //with the overarching name, navItems, using curly braces to add in the name of the 
 //nav items first then the link that would link the name with the link (Navigating Menu)
 
-{name: "Home", link: "index.html"},
-{name: "Menu", link: "menu.html"},
-{name: "Contact", link: "contact.html"},
-{name: "About", link: "about.html"}
+{ name: "Home", link: "index.html"},
+{ name: "Menu", link: "menu.html"},
+{ name: "Contact", link: "contact.html"},
+{ name: "About", link: "about.html"}
     
 //Use Shift Alt to duplicate a selection that you highlighted and copied then use
     //the down arrow key to duplicate.
@@ -17,7 +17,18 @@ const navItems = [
 ];
 
 //Getting the list where we're going to add the Nav items.
-const navList = document.getElementById("navBar");
+// const navList = document.getElementById("navBar");
+
+const navList = document.querySelector("nav ul");
+
+function handleClicks(event) {
+    //Prevent Default
+    event.preventDefault();
+    //Get href attribute.
+    const pageLink = this.getAttribute("href");
+    //Open the Page Link.
+    window.location.href = pageLink;
+}
 
 //Now we want to get a function to create the NavBar Items
 function makeNav() {
@@ -31,7 +42,9 @@ function makeNav() {
         //Now we're creating a h-ref for the link to make the element "A" work as a link.
         link.setAttribute("href", item.link);
         //Now we're going to get the Names for the Link (Text context for the menu)
-        listItem.textContent = item.name;
+        link.textContent = item.name;
+        //Adding the Click Event for the Link in the Nav.
+        link.addEventListener("click", handleClicks);
         //In the Navbar, we're going to add the link to the listItems.
         //Side Comment, the append function is similar to concatentating two different
         //things together by combining them so we're adding a link to the list items
@@ -39,7 +52,7 @@ function makeNav() {
         listItem.appendChild(link);
         //Now we're going to add the list item to the Nav items.
         navList.appendChild(listItem);
-    });
+        });
 }
 //Now we're going to use the function (invoke it/call it) to create the nav bars.
 makeNav();
